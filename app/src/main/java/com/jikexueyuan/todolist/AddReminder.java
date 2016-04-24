2a8c6e6dcd1d573cacd29fb9e5f92ea4d6e0fda8
RogerSun <hehedehehe@gmail.com>
2016-04-24 16:43:37 +0800

    MacPro: 保存和查询，完成

--- app/src/main/java/com/jikexueyuan/todolist/AddReminder.java
+++ app/src/main/java/com/jikexueyuan/todolist/AddReminder.java
@@ -1,14 +1,18 @@
 package com.jikexueyuan.todolist;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddReminder extends Activity {
 
     private EditText etClock;
     private EditText etReminder;
+
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,38 @@ public class AddReminder extends Activity {
         findViewById(R.id.btnSave).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String clock = etClock.getText().toString();
                 String reminder = etReminder.getText().toString();
 
+                int clock_int = Integer.parseInt(clock);
 
+                if (clock_int >= 0 && clock_int <= 24){
+                    db = SQLiteDatabase.openOrCreateDatabase("/data/user/0/com.jikexueyuan.todolist/files/reminder.db3",null);
+
+                    insertData(db,clock,reminder);
+
+                    setResult(0);
+
+                    Toast.makeText(AddReminder.this,"Saved successfully",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    Toast.makeText(AddReminder.this,"Please input a clock between 0 - 24.",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    //插入数据
+    private void insertData(SQLiteDatabase db, String clock, String reminder){
+        db.execSQL("insert into event values(null,?,?)", new String[] {clock,reminder});
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (db != null && db.isOpen()){
+            db.close();
+        }
     }
 }
--- app/src/main/java/com/jikexueyuan/todolist/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/todolist/MainActivity.java
@@ -1,27 +1,29 @@
 package com.jikexueyuan.todolist;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
     ListView listView;
+    String dbPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = SQLiteDatabase.openOrCreateDatabase(this.getFilesDir().toString()+"reminder.db3",null);
-
-        listView = (ListView) findViewById(R.id.listView);
-
+        refreshList();
 
     }
 
@@ -38,12 +40,64 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.addEvent:
                 Intent reminderIntent = new Intent(this,AddReminder.class);
+                //reminderIntent.putExtras("Path",1);
                 startActivityForResult(reminderIntent,0);
+
                 break;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    //DB相关
+    private void refreshList (){
+        dbPath = this.getFilesDir().toString();
+
+        db = SQLiteDatabase.openOrCreateDatabase(dbPath + "/reminder.db3",null);
+
+        System.out.println(dbPath + "/reminder.db3");
+
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        try{
+            Cursor cursor = db.rawQuery("select * from event", null);
+            inflateList(cursor);
+        }catch(SQLiteException se){
+            db.execSQL("create table event (_id integer" + " primary key autoincrement, " + "clock varchar(50)," + " reminder varchar(255))");
+        }
+    }
+
+    private void inflateList(Cursor cursor){
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                MainActivity.this,
+                R.layout.line, cursor,
+                new String[] { "clock", "reminder" },
+                new int[] {R.id.clock, R.id.reminder },
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (db != null && db.isOpen()){
+            db.close();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case 0:
+                switch (resultCode){
+                    case 0:
+                        refreshList();
+                        break;
+                }
+                break;
+        }
+    }
 }
